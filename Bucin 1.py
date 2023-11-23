45a992a662b2ce32f61b639ffed93be7321c41fe
faishalnafi <faishalnafi50@gmail.com>
2023-11-23 09:05:34 +0700

    V231

---
+++ Bucin 1.py
@@ -0,0 +1,22 @@
+#BUCIN CLASS PROGRAM
+#Created By FaishalNafi'
+
+#variabel
+mau = "Y"
+gak = "N"
+
+#Input
+print("Apakah kakmu suka padaku?")
+masuk = str(input("Jawab[Y/N]: "))
+
+#Program
+while masuk != mau:
+    print("Jawabannya hanya ada mau ajah :)")
+    print("Apakah kamu suka padaku?")
+    masuk = str(input("Jawab[Y/N]: "))
+    
+    if masuk == mau:
+        break
+    
+print("Yeah besok kita nikah !!")
+print("'Love You' from Napikk") #End
